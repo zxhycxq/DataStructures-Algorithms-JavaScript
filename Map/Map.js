95bbe77a24a2170fd6e17c041803c110cc55c088
zxhycxq <lcyxlqn@163.com>
2018-05-09 07:57:39 +0800

    add map

---
+++ Map/Map.js
@@ -0,0 +1,52 @@
+function Dictionary() {
+  var items={};
+  
+  this.has =function (key) {
+    return key in items;
+  }
+  
+  this.set=function (key,value) {
+    items[key]=value;
+  }
+  
+  this.delete=function (key) {
+    if(this.has(key)){
+      delete items[key];
+      return true
+    }
+    return true;
+  }
+  
+  this.get =function (key) {
+    return this.has(key)?items[key]:undefined;
+  }
+  
+  this.values=function () {
+    var values=[];
+    for(var k in items){
+      if(this.has(k)){
+        values.push(items[k]);
+      }
+    }
+    return values;
+  }
+  
+  this.keys=function () {
+    return Object.keys(items);
+  }
+  
+  this.getItems=function () {
+    return items;
+  }
+}
+
+var dictionary = new Dictionary();
+dictionary.set('001', '张三');
+dictionary.set('002', 'jlisi i');
+dictionary.set('003', '王五');
+console.log(dictionary.has('003'));
+// console.log(dictionary.size());
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.getItems());
+console.log(dictionary.get('001'));
