b52ad9bb90b79951b2ac9435011814cf5fae7479
zxhycxq <lcyxlqn@163.com>
2018-05-15 20:55:55 +0800

    add 归并排序

--- Sort/Sort.js
+++ Sort/Sort.js
@@ -54,6 +54,39 @@ function ArrayList() {
       array[j] = temp;
     }
   }
+  
+  this.mergeSort=function () {
+    array = mergeSortRec(array)
+  }
+  
+  var mergeSortRec=function (array) {
+    var length=array.length;
+    if(length===1){
+      return array
+    }
+    var mid=Math.floor(length/2),
+        left=array.slice(0,mid),
+        right=array.slice(mid,length);
+    return merge(mergeSortRec(left),mergeSortRec(right))
+  }
+  
+  var merge=function (left,right) {
+    var result=[],il=0,ir=0;
+    while (il<left.length&&ir<right.length){
+      if(left[il]<right[ir]){
+        result.push(left[il++]);
+      }else{
+        result.push(left[ir++]);
+      }
+      while (il<left.length){
+        result.push(left[il++]);
+      }
+      while (ir<left.length){
+        result.push(left[ir++]);
+      }
+      return result;
+    }
+  }
 }
 
 
