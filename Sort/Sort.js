a8980bc88f532c4b0192f205d2159d7902a30587
zxhycxq <lcyxlqn@163.com>
2018-05-17 08:00:43 +0800

    add 二分搜索、顺序搜索

--- Sort/Sort.js
+++ Sort/Sort.js
@@ -124,7 +124,37 @@ function ArrayList() {
     }
     return i;
   }
-  
+  // 顺序搜索
+  this.sequeuntialSearch=function (item) {
+    for(var i=0;i<array.length;i++){
+      if(item = array[i]){
+        return i;
+      }
+    }
+    return -1;
+  }
+//  二分搜索
+  this.binarySearch=function (item) {
+    this.quickSort();
+    var low=0,high=array.length-1,mid,element;
+    while (low<high){
+      mid = Math.floor(low+high/2);
+      element=array[mid];
+      if(element<item){
+        low=mid+1;
+      }else if(element>item){
+        high=mid-1;
+      }else{
+        return mid;
+      }
+    }
+     return -1;
+  }
+
+
+
+
+
 }
 
 
